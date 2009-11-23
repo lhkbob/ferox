a735c0ae7d7bdfbd605f99bd608ca736cda5a612
lhkbob <devnull@localhost>
2009-11-23 18:23:11 +0000

    No longer needed

--- ferox-jogl-v1/src/com/ferox/renderer/impl/jogl/record/GlslShaderRecord.java
+++
@@ -1,8 +0,0 @@
-package com.ferox.renderer.impl.jogl.record;
-
-public class GlslShaderRecord {
-	public boolean enableVertexProgramTwoSide = false;
-	public boolean enableVertexProgramPointSize = false;
-
-	public int glslProgramBinding = 0;
-}
