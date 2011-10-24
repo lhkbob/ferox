60d88f1331b842208d8739e5c6688a295a61c322
lhkbob <devnull@localhost>
2011-10-24 22:24:23 +0000

    Remove project that has been moved to new SCM

--- ferox-entity/test/com/ferox/entity/property/FloatPropertyFactory.java
+++
@@ -1,8 +0,0 @@
-package com.ferox.entity.property;
-
-public class FloatPropertyFactory implements PropertyFactory<FloatProperty> {
-    @Override
-    public FloatProperty create() {
-        return new FloatProperty(1);
-    }
-}
