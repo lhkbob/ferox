aa16c25566e84fff55d2844dd2bec5eb349e44ec
Michael Ludwig <lhkbob@gmail.com>
2013-01-02 18:37:21 -0600

    Add topology conversion method signatures (no implementation yet)

---
+++ ferox-renderer/ferox-renderer-api/src/main/java/com/ferox/util/geom/TopologyUtil.java
@@ -0,0 +1,36 @@
+package com.ferox.util.geom;
+
+import com.ferox.resource.VertexBufferObject;
+
+public final class TopologyUtil {
+    private TopologyUtil() {}
+
+    public static void inflateTriangleStrip(VertexBufferObject strips, int stripOffset,
+                                            int stripCount,
+                                            VertexBufferObject targetTris, int triOffset) {
+
+    }
+
+    public static void inflateQuadStrip(VertexBufferObject strips, int stripOffset,
+                                        int stripCount, VertexBufferObject targetQuads,
+                                        int quadOffset) {
+
+    }
+
+    public static void inflateTriangleStripArray(int stripOffset, int stripCount,
+                                                 VertexBufferObject targetTris,
+                                                 int triOffset) {
+
+    }
+
+    public static void inflateQuadStripArray(int stripOffset, int stripCount,
+                                             VertexBufferObject targetQuads,
+                                             int quadOffset) {
+
+    }
+
+    public static void inflateSimpleArray(int stripOffset, int stripCount,
+                                          VertexBufferObject target, int targetOffset) {
+
+    }
+}
