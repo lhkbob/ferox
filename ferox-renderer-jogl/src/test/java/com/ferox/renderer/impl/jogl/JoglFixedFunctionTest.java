53b8f01ea67e3901ec4949ccd9d0a2e123f00484
Michael Ludwig <lhkbob@gmail.com>
2013-08-08 01:09:23 -0500

    Fix package name in jogl tests

--- ferox-renderer-jogl/src/test/java/com/ferox/renderer/impl/jogl/JoglFixedFunctionTest.java
+++ ferox-renderer-jogl/src/test/java/com/ferox/renderer/impl/jogl/JoglFixedFunctionTest.java
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package lwjgl;
+package com.ferox.renderer.impl.jogl;
 
 import com.ferox.math.Matrix4;
 import com.ferox.math.Vector3;
--- ferox-renderer-jogl/src/test/java/com/ferox/renderer/impl/jogl/JoglSelfDestructTest.java
+++ ferox-renderer-jogl/src/test/java/com/ferox/renderer/impl/jogl/JoglSelfDestructTest.java
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package lwjgl;
+package com.ferox.renderer.impl.jogl;
 
 import com.ferox.renderer.*;
 
